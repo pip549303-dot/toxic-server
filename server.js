9208c0bb34a4a49e614fc805a20c48fbb545f64a
pip549303-dot <pip549303@gmail.com>
2026-04-13 19:25:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 const express = require("express");
-const ytdl = require("ytdl-core");
+const ytdl = require("@distube/ytdl-core");
 
 const app = express();
 
-// الصفحة الرئيسية
 app.get("/", (req, res) => {
   res.send("Server is running ✅");
 });
 
-// التحميل
-app.get("/download", (req, res) => {
+app.get("/download", async (req, res) => {
+  try {
     const url = req.query.url;
 
     if (!ytdl.validateURL(url)) {
-        return res.send("Invalid URL");
+      return res.send("Invalid URL");
     }
 
     res.header("Content-Disposition", 'attachment; filename="video.mp4"');
 
-    ytdl(url).pipe(res);
+    ytdl(url, {
+      quality: "highest",
+      filter: "audioandvideo"
+    }).pipe(res);
+
+  } catch (err) {
+    res.send("Error: " + err.message);
+  }
 });
 
 app.listen(10000, () => {
-    console.log("Server running");
+  console.log("Server running");
 });
